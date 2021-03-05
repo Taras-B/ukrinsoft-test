a1819b986fc863d5b270017b51993a4b86f04814
Taras-B <taras5475@gmail.com>
2021-03-05 15:46:33 +0200

    Create cart component

---
+++ src/pages/Orders/components/Cart.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+
+import { RootState } from '../../../store/rootReducer'
+import { actionsOrders } from '../../../store/orders/actions'
+
+export const Cart = () => {
+  const dispatch = useDispatch()
+  const cart = useSelector((state: RootState) => state.orders.cart)
+
+  const onSendToConfirm = () => {
+    if (cart.length >= 1) {
+      dispatch(actionsOrders.confirmOrder())
+    }
+  }
+
+  if (cart.length < 1) {
+    return (
+      <Grid item xs={12}>
+        <Box textAlign='center'>
+          <Typography variant='h6'>Cart is empty</Typography>
+        </Box>
+      </Grid>
+    )
+  }
+  return (
+    <>
+      <Grid container item spacing={3}>
+        {cart.map((item) => (
+          <Grid item xs={12} sm={6} key={item}>
+            <Paper elevation={14}>
+              <Box textAlign='center' py={1}>
+                <Typography variant='body1'>{item}</Typography>
+              </Box>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+      <Grid container item xs={12} justify='center'>
+        <Box mt={2}>
+          <Button color='primary' onClick={onSendToConfirm}>
+            Confirm
+          </Button>
+        </Box>
+      </Grid>
+    </>
+  )
+}
--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -3,10 +3,6 @@ import { useSelector, useDispatch } from 'react-redux'
 
 import Grid from '@material-ui/core/Grid'
 import { makeStyles } from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardHeader from '@material-ui/core/CardHeader'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
@@ -14,6 +10,7 @@ import Paper from '@material-ui/core/Paper'
 import { Box } from '@material-ui/core'
 import { RootState } from '../../store/rootReducer'
 import { actionsOrders } from '../../store/orders/actions'
+import { Cart } from './components/Cart'
 
 export const Orders = () => {
   const dispatch = useDispatch()
@@ -33,37 +30,17 @@ export const Orders = () => {
             <Typography variant='h4'>Cart</Typography>
           </Box>
         </Grid>
-        {cart.length >= 1 ? (
-          <>
-            <Grid container item spacing={3}>
-              {cart.map((item) => (
-                <Grid item xs={12} sm={6}>
-                  <Paper elevation={14} key={item}>
-                    <Box textAlign='center' py={1}>
-                      <Typography variant='body1'>{item}</Typography>
-                    </Box>
-                  </Paper>
-                </Grid>
-              ))}
-            </Grid>
-            <Grid container item xs={12} justify='center'>
-              <Box mt={2}>
-                <Button color='primary' onClick={onSendToConfirm}>
-                  Confirm
-                </Button>
-              </Box>
-            </Grid>
-          </>
-        ) : (
-          <Grid item xs={12}>
-            <Box textAlign='center'>
-              <Typography variant='h6'>Cart is empty</Typography>
-            </Box>
-          </Grid>
-        )}
+        <Cart />
       </Grid>
-      <Grid item xs={12}>
-        <Typography variant='h4'>Orders</Typography>
+      <Grid item xs>
+        <Divider />
+      </Grid>
+      <Grid container item xs={12}>
+        <Grid item xs={12}>
+          <Box textAlign='center'>
+            <Typography variant='h4'>Orders</Typography>
+          </Box>
+        </Grid>
       </Grid>
     </Grid>
   )
